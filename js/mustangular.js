ad50af7465304438d047eaf6ebd0f67bdf207a80
Kyla Marczewski <kyla.m.ski@gmail.com>
2016-07-14 16:10:20 -0700

    Added logic to change colors of icons (incomplete), fixed ability to show single metrics, added metric select, added colored icons

--- js/mustangular.js
+++ js/mustangular.js
@@ -1,7 +1,23 @@
 //Form 
 var app = angular.module('myApp', []);
-app.controller('formCtrl', function($scope, $window, $httpParamSerializer, $location){
+app.controller('formCtrl', function($scope, $window, $httpParamSerializer, $location, $http){
+  
+  $scope.metrics = [];
+  $http.jsonp("http://service.iris.edu/mustang/metrics/1/query?output=jsonp&callback=angular.callbacks._0", {cache:true}).success(function(data, status, headers, config){ 
+    
+    for(var i = 0; i<data.metrics.length; i++){
+      $scope.metrics[i] = {
+        "metric" : data.metrics[i].name,
+        "title" : data.metrics[i].title
+      } 
+    } 
+    
+  }).error(function(data, status, headers, config){ //Doesn't get triggered if the metric array is empty or an error
+    // console.log(data + " : " + status)
+  });
+
   $scope.master = {};
+  
   $scope.submit = function(params) {
     $scope.master = angular.copy(params);
     // console.log(params);
@@ -19,20 +35,76 @@ var app2 = angular.module('myApp2', ['leaflet-directive'], function($locationPro
   });
   
 })
-.service('metricsList', function($filter){
+.service('iconColoring', function($filter){
+  var _max = -1000; //values get wiped out
+  var _min = 1000;
+  var _binCount = 3;
+  var _binWidth = (_max - _min) / _binCount;
+  
+  var findBounds = function (){
+    
+  }
+  
+  this.getValue = function(chans){
+    // console.log(station.chans)
+    var max = 0;
+    var count = Object.keys(chans).length;
+    for(var i = 0; i < count; i++){ //find the median within the station and most extreme of the channels
+      var array = $filter('orderBy')(chans[Object.keys(chans)[i]]);
+      var mid = count/2+.5;
+      if(mid % 1 == 0){
+        max = Math.max(max, array[mid]);
+      } else {
+        max = Math.max(max, (array[mid-.5]+array[mid-.5])/2);
+      }
+      
+    }
+    console.log(max)
+    return max;
+  }
+  
+  this.getIcon = function(value){
+    var icon;
+    this.setMax(value);
+    this.setMin(value);
+    if(value > 100){
+      icon = "images/red.png"
+    } else if (value > 50){
+      icon = "images/yellow.png"
+    } else if (value){
+      icon = "images/green.png"
+    } else {
+      icon = "images/dot.png"
+    }
+    
+    return icon;
+    
+  }
+  
+  this.getMessage = function(station, value){
+    // console.log(station.chans)
+    
+    return "<ul>" + "<li>" + value+ "</li>"+ "<li>" + station.net + "</li>"+"</ul>"   
+  }
+  
+  this.setMax = function(max){
+    _max = max;
+  }
+  this.setMin = function(min){
+    _min = min;
+  }
+  this.getMax = function(){
+    return _max;
+  }
+  this.getMin = function(){
+    return _min;
+  }
+})
+
+.factory('metricsList', function($filter){
   var _metrics = [];
   var _stations = [];
   var _combined = [];
-  // this.metrics = _metrics;
-
-
-  //
-  // var filterStations = function(){
-  //   console.log("wheee");
-  //   for(var i = 0; i < _metrics.length; i++){
-  //     console.log($filter('filter')(_stations, {station: _metrics[i].sta}, true)[0]);
-  //   }
-  // }
   
   return{
     getMetrics: function(){
@@ -44,64 +116,58 @@ var app2 = angular.module('myApp2', ['leaflet-directive'], function($locationPro
     setMetrics: function(value){
       _metrics = value;
     },
-    setStations: function(i, value){
-      _stations[i] = value;
+    setStations: function(value, key){
+      _stations[key] = value;
     },
     getStations: function(){
       return _stations;
     },
     combineLists: function(){
       // console.log("combine")
-      
-
         for(var i = 0; i < _metrics.length; i++){
-          // console.log(i)
-          var sta = $filter('filter')(_stations, {station: _metrics[i].sta}, true)[0];
-          var metric = _metrics[i];
-          metric.lat = sta.latitude;
-          metric.lng = sta.longitude;
-          metric.elev = sta.elevation;
-          if(_combined[metric.sta]){
-            var message = _combined[metric.sta].message;
-            if(message.indexOf(metric.chan)> -1){
-              // message = message.split(" ");
-              // var place = message.indexOf(metric.chan )> -1
-              // message.indexOf(metric.chan)> -1
-              // console.log(place)
-              var chan = metric.chan;
-              var re = new RegExp(chan + ":\\s{1}\\d{1,4}", 'g');
-              var s = message.match(re);
-              var max = 0;
-              for(var k = 0; k < s.length; k++){
-                var f = parseFloat(s[k].split(" ")[1]);
-                max = Math.max(max, f);
-              }
-              // Replace old value with higher value for the channel
-              // _combined[metric.sta].message 
-              console.log(max)
-              
-            } else {
-              _combined[metric.sta].message = message + "\n " + metric.chan + ": " + metric.value
-            }
+          var sta = _stations[_metrics[i].sta+"_"+_metrics[i].net]
+          if(sta && sta.latitude && sta.longitude){ //only plot if it has a latitude and a longitude
+            var metric = _metrics[i];
+            metric.lat = sta.latitude; //print out table of failed stations --> no data
+            metric.lng = sta.longitude;
+            metric.elev = sta.elevation;
             
-          } else{
-            _combined[metric.sta] = { //+"_"+_metrics[i].chan
-              lat: parseFloat(metric.lat),
-              lng: parseFloat(metric.lng),
-              message: metric.chan + ": " + metric.value
+            if(_combined[metric.sta]){
+              // _combined[metric.sta].value = Math.max(_combined[metric.sta].value, metric.value)
+              if(_combined[metric.sta].chans[metric.chan]){
+                _combined[metric.sta].chans[metric.chan].push(metric.value); //need the median value
+              } else {
+                _combined[metric.sta].chans[metric.chan] = [metric.value];
+              }
+            } else{
+              _combined[metric.sta] = { //+"_"+_metrics[i].chan
+                sta: metric.sta,
+                lat: parseFloat(metric.lat),
+                lng: parseFloat(metric.lng),
+                net: metric.net,
+                chans:{}
+                // focus: true
+              }
+              // console.log(metric.chan)
+              _combined[metric.sta].chans[metric.chan] = [metric.value];
+              // console.log(_combined[metric.sta])
             }
+          } else {
+            //put it in a table
           }
-        }
+      }
       
     },
     getCombined: function(){
       return _combined;
     }
   }
-});
+})
 // add service to allow sharing of stations between controllers
 
-app2.controller("SimpleMapController", [ '$scope', '$window', '$http', 'metricsList', function($scope, $window, $http, metricsList) {
+  
+
+app2.controller("SimpleMapController", function($scope, $window, $http, metricsList, iconColoring) {
   angular.extend($scope, {
       markers: {
         
@@ -110,60 +176,73 @@ app2.controller("SimpleMapController", [ '$scope', '$window', '$http', 'metricsL
           scrollWheelZoom: false
       }
   });
-  // $scope.layers = {
-  //   baselayers: {},
-  //   overlays: {}
-  // }
   var params = $window.location.search;
   var url = 'http://service.iris.edu/mustang/measurements/1/query';
-  var configs = '&timewindow=2016-05-30,2016-05-31' + '&metric=data_latency&output=jsonp&callback=angular.callbacks._0';
+  var configs = '&timewindow=2016-05-30,2016-05-31' + '&output=jsonp&callback=angular.callbacks._0';
   
-  $http.jsonp(url + params + configs).success(function(data, status, headers, config){
-    // data.measurments
-    // console.log(data.measurements.data_latency)
+  $http.jsonp(url + params + configs, {cache:true}).success(function(data, status, headers, config){ //TODO: don't do this caching in prod
+    console.log(url + params + configs)
     metricsList.setMetrics(data.measurements.data_latency);
-    // metricsList.setMetrics(data.measurements);
-    // console.log(metricsList.getMetrics()[0]);
-    metricsList.combineLists();
-    // console.log(metricsList.getProperty())
-    var markers1 = metricsList.getCombined();
-    console.log(markers1);
-    $scope.markers = markers1;
 
-  }).error(function(data, status, headers, config){ //Doesn't get triggered if the metric array is empty
+    $scope.stations=[];
+    var data;
+    //net, sta, loc, chan, start, end, 
+    params = params.replace(/metric=\w*/ig,'')
+    params = params.replace(/chan/ig,'cha')
+    $http.get('http://service.iris.edu/fdsnws/station/1/query'+params+'&format=text').then(function success(response){
+      // console.log(response.data);
+      data = response.data.split('\n'); //Oth is the header
+      if(data[0].length > 0){
+        var headers = data[0].split("#");
+        headers = headers[1].split(" | ");
+        // console.log(headers);
+        console.log("in get")
+        for (var i = 1; i < data.length; i++){
+         // console.log(data.length-i)
+          var station = data[i].split("|");
+          var staObj = {};   
+          for (var j = 0; j < station.length; j++){
+            staObj[headers[j].trim().toLowerCase()]=station[j];
+          }
+          $scope.stations.push(staObj);
+          metricsList.setStations(staObj, station[1]+"_"+station[0]);
+        }    
+        console.log("combine")
+        metricsList.combineLists();
+        var markers1 = metricsList.getCombined();
+        var markers2 = [];
+      
+        for(var i = 0; i < Object.keys(markers1).length; i++) {
+          var m = markers1[Object.keys(markers1)[i]];
+          var value = iconColoring.getValue(m.chans);
+          markers2[i]={
+            lat: m.lat,
+            lng: m.lng,
+            message: iconColoring.getMessage(m, value),
+            icon: {
+              iconUrl: iconColoring.getIcon(value),
+              iconSize: [10, 10]
+            }
+          }
+
+        }
+
+        $scope.markers = markers2;
+        $scope.maxValue = iconColoring.getMax;
+        $scope.minValue = iconColoring.getMin;
+      }
+    }, function error(response){
+      // console.log(response);
+    });
+  }).error(function(data, status, headers, config){ //Doesn't get triggered if the metric array is empty or an error
     // console.log(data + " : " + status)
   });
-  
-  $scope.stations=[];
-  var data;
-  //net, sta, loc, chan, start, end, 
-  $http.get('http://service.iris.edu/fdsnws/station/1/query?network=UW&format=text').then(function success(response){
-    // console.log(response.data);
-    data = response.data.split('\n'); //Oth is the header
-    
-    var headers = data[0].split("#");
-    headers = headers[1].split(" | ");
-    
-    for (var i = 1; i < data.length; i++){
-      var station = data[i].split("|");
-      var staObj = {};   
-      for (var j = 0; j < station.length; j++){
-        staObj[headers[j].trim().toLowerCase()]=station[j];
-      }
-      $scope.stations.push(staObj);
-      metricsList.setStations(i-1, staObj);
-    }    
-    metricsList.combineLists();
-    // console.log(metricsList.getStations());
-    
-  }, function error(response){
-    // console.log(response);
-  });
+
 
 
   
   
-}]);
+});
 
 //Disable default debugging output on leaflet
 app2.config(function($logProvider){
